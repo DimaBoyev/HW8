28db27223444b799ca979d8609da8be2c4438e73
DimaBoyev <dmitriy.boyev@gmail.com>
2020-03-17 11:41:15 +0200

    HW updated

--- src/infrastructure/logger/Logger.java
+++ src/infrastructure/logger/Logger.java
@@ -1,17 +1,6 @@
 package infrastructure.logger;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public abstract class Logger {
-    private static int counter = 1;
 
-    public static void log(String message) {
-        final String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-        DateFormat dateFormat = new SimpleDateFormat("HH:MM:ss:SSS");
-        Date date = new Date();
-        String dateNow = dateFormat.format(date);
-        System.out.printf("%d) %s [%s]: %s", counter++, dateNow, methodName, message);
-    }
+      abstract void log(String message);
 }
--- src/infrastructure/logger/StdLogger.java
+++ src/infrastructure/logger/StdLogger.java
@@ -1,5 +1,9 @@
 package infrastructure.logger;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public final class StdLogger extends Logger {
     private static StdLogger instance = null;
 
@@ -14,8 +18,26 @@ public final class StdLogger extends Logger {
         return instance;
     }
 
+    private static int counter = 1;
+
+    void log(String message) {
+        final String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+        DateFormat dateFormat = new SimpleDateFormat("HH:MM:ss:SSS");
+        Date date = new Date();
+        String dateNow = dateFormat.format(date);
+        System.out.printf("%d) %s [%s]: %s\n", counter++, dateNow, methodName, message);
+    }
+
     private static void toConsole(String msg) {
-        log(msg);
+        StdLogger logToConsole = new StdLogger();
+        logToConsole.log(msg);
+    }
+
+    public static void main(String[] args) {
+        toConsole("loh");
+        toConsole("lohnes");
+        StdLogger kk = new StdLogger();
+        kk.log("Lupito");
     }
 
 }
